f91ee51839ce444adc5c250a96bf5109456c39a9
Anas TULIMAT <anas.tulimat@orange.com>
2024-03-06 15:57:32 +0100

    feat: add benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "./components/Header.jsx";
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
 
-import './App.css'
 import Hero from "./components/hero/Hero.jsx";
+import Benefits from "./components/Benefits.jsx";
+
+import './App.css'
 
 //----------------------------------------------
 
@@ -13,6 +15,7 @@ const App = () => {
             <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
                 <Header/>
                 <Hero />
+                <Benefits />
             </div>
 
             <ButtonGradient/>
