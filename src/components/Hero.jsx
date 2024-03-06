3ea8b109efff2fc952dc7d2d4980a2764fc02986
Anas TULIMAT <anas.tulimat@orange.com>
2024-03-06 14:54:08 +0100

    feat: add hero image into hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from "./Button";
 import Section from "./Section.jsx";
 
-import {curve} from "../assets";
+import {curve, robot, heroBackground} from "../assets";
 
 //----------------------------------------------
 
@@ -41,6 +41,37 @@ const Hero = () => {
                     <Button href="/pricing" white>
                         Get started
                     </Button>
+                </div>
+
+                <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+                    <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+                        <div className="relative bg-n-8 rounded-[1rem]">
+
+                            <div
+                                className="aspect-[33/40] rounded-[1rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]"
+                            >
+                                <img
+                                    src={robot}
+                                    className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                                    width={1024}
+                                    height={490}
+                                    alt="AI"
+                                />
+                            </div>
+                        </div>
+                    </div>
+
+                    <div
+                        className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]"
+                    >
+                        <img
+                            src={heroBackground}
+                            className="w-full"
+                            width={1440}
+                            height={1800}
+                            alt="hero"
+                        />
+                    </div>
                 </div>
             </div>
 
